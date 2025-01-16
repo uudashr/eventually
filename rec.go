a16c944db06dd1589ca13765c532381002f3da45
Nuruddin Ashr <uudashr@gmail.com>
2025-01-16 13:50:42 +0700

    Provide construction function for Recorder

--- rec.go
+++ rec.go
@@ -6,6 +6,11 @@ type Recorder struct {
 	Events []Event
 }
 
+// NewRecorder allocates and returns a new Recorder.
+func NewRecorder() *Recorder {
+	return &Recorder{}
+}
+
 // Publish the event to the recorder.
 func (r *Recorder) Publish(event Event) {
 	r.Events = append(r.Events, event)
